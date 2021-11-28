c261fb65c808cd948efd5041ec6803d421067dd7
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-11-27 23:22:05 -0500

    💻CLI messages structure added

--- src/api/requests.rs
+++ src/api/requests.rs
@@ -1,20 +1,15 @@
 use reqwest;
 
-pub fn make_request() {
-    println!("This is a request");
+pub fn echo_request(echo: &str) {
+    println!("{}", echo);
 }
 
 pub async fn random_name_request() -> (Result<(), reqwest::Error>, String) {
-    println!("Stuff");
     let res = reqwest::get("https://random-names-api.herokuapp.com/random")
         .await
         .unwrap();
 
-    println!("Status: {}", res.status());
-
     let body = res.text().await.unwrap();
-
-    println!("Body:\n\n{}", body);
 
     (Ok(()), body)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod api;
+mod cli;
 use futures::executor::block_on;
 
 #[tokio::main]
 async fn main() {
-    api::requests::make_request();
+    cli::terminal_interface::start();
+    api::requests::echo_request("Initializing API...");
     let (_, x): (_, String) = block_on(api::requests::random_name_request());
     api::data_processing::get_random_name(&x);
 }
